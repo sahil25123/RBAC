699adaf1cd42a913a640e609c1ee24cd9d2a059c
sahil25123 <sahilgupta25123@gmail.com>
2025-11-08 16:37:19 +0530

    Implement authentication controllers and routes for login and registration

---
+++ backend/controllers/authController.js
@@ -0,0 +1,40 @@
+import prisma from "../config/prisma";
+
+export const loginController = async(req , res) =>{
+    try{
+        const {email , password} =req.body;
+
+        if(!email || password){
+            return res.status(400).json({message : "Missing Fields"})
+
+        }
+        const existing = prisma.user.findUnique({})
+
+    }
+    catch(e){
+        return res.status(404).json({message: "Error in the Login Controller"})
+    }
+
+
+}
+
+export const registerController = async(req, res) =>{
+
+    try {
+    
+        const { name , email , password } = req.body;
+        if(!name || email || password) {
+            res.status(400).json({message : "Missing Fields"})
+        }
+        const existing = await prisma.user.findUnique({where : {email}});
+        const salt = await bcrypt.genSalt(10);
+        const hashpassword = await bcrypt.hash(password , salt)
+
+
+    }
+    catch(e){
+        console.log("")
+            return res.status(404).json({message : "Error in the Register Controller"})
+    }
+
+}
